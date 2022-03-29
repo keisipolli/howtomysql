f5953e0b9a11b3d23e7c57e0c15df0e77563daf9
Keisi Polli <keisi.polli@khk.ee>
2022-03-29 16:13:15 +0300

    Join Two or More Tables

--- db.js
+++ db.js
@@ -9,9 +9,10 @@ var con = mysql.createConnection( {
 
 con.connect(function(err) {
     if (err) throw err;
-    var sql = "SELECT * FROM customers LIMIT 5 OFFSET 2";
+    var sql = "SELECT users.name AS user, products.name AS favorite FROM users JOIN products ON users.favorite_product = products.id";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log(result);
     });
 });
+
